b2e7458eea2f025b1f6702829a7bd354d4d4b888
Steffen Müller <steffen@mllrsohn.com>
2014-01-31 22:02:15 +0100

    added generators

--- Gulpfile.js
+++ Gulpfile.js
@@ -2,6 +2,7 @@
 // Require the needed packages
 var lr = require('tiny-lr');
 var gulp = require('gulp');
+var fs = require('fs');
 var stylus = require('gulp-stylus');
 var rimraf = require('gulp-rimraf');
 var browserify = require('gulp-browserify');
@@ -11,6 +12,9 @@ var lrserver = lr();
 var spawn = require('child_process').spawn;
 var karma = require('gulp-karma');
 var protractor = require('gulp-protractor');
+var path = require('path');
+var mkdirp = require('mkdirp');
+var handlebars = require('handlebars');
 
 // Variables
 var paths = {
@@ -67,16 +71,6 @@ gulp.task('nodemon', function(cb) {
 });
 
 
-// webdriver setup and run
-gulp.task('webdriver', function(cb) {
-    spawn('./node_modules/.bin/webdriver-manager', ['update'], { stdio: 'inherit' }).on('close', function() {
-        spawn('./node_modules/.bin/webdriver-manager', ['start'], { stdio: 'inherit' }).on('close', function() {
-            cb();
-        });
-    });
-});
-
-
 
 // Setting up the default Task
 gulp.task('default', function() {
@@ -118,11 +112,132 @@ gulp.task('karma', function() {
 
 
 // Setting up the test task
-gulp.task('protractor', function() {
-    gulp.src(['./test/client/e2e/**/*.js']).pipe(protractor({
-        configFile: './test/client/protractor.conf.js'
-    }));
+gulp.task('g', function(cb) {
+    var basePath = './public/app';
+    var basePathTest = './test/client/specs';
+    var generators = './generators';
+    var baseApp = 'mllrsohn';
+    var n;
+
+    var scaffold = {
+        module: function (name) {
+            ['controllers', 'providers', 'directives', 'filters', 'templates'].forEach(function(item) {
+                mkdirp.sync(path.join(basePath, name, item));
+            });
+
+            ['controllers', 'providers', 'directives', 'filters'].forEach(function(item) {
+                mkdirp.sync(path.join(basePathTest, name, item));
+            });
+
+        },
+        checkName: function () {
+            var n = gulp.env.name.split(':');
+            if(n.length !== 2) {
+                console.log('Please Provide the name as in module:name. eg: admin:list');
+                return cb();
+            } else {
+                return n;
+            }
+        },
+
+        compile: function (template, vars, mpath) {
+            mkdirp.sync(mpath);
+            vars.fn = path.join(mpath, vars.filename);
+            var gc = fs.readFileSync(path.join(generators, template));
+            var t = handlebars.compile(gc.toString());
+            fs.writeFileSync(vars.fn, t(vars));
+        },
+        index: function (module) {
+            scaffold.compile('index.hbs', {
+                module: module,
+                filename: 'index.js',
+                base: baseApp
+            }, path.join(basePath, module), 'index');
+        },
+        controller: function (module, name) {
+            scaffold.compile('controller.hbs', {
+                module: module,
+                name: name,
+                filename: name+'Ctr.js'
+            }, path.join(basePath, module, 'controllers'));
+        },
+        controllerTest: function (module, name) {
+            scaffold.compile('controller_test.hbs', {
+                module: module,
+                name: name,
+                base: baseApp,
+                filename: name+'Ctr.js'
+            }, path.join(basePathTest, module, 'controllers'));
+        },
+        template: function (module, name) {
+            scaffold.compile('template.hbs', {
+                module: module,
+                filename: name+'.html'
+            }, path.join(basePath, module, 'templates'));
+        },
+        directive: function (module, name) {
+            scaffold.compile('directive.hbs', {
+                module: module,
+                name: name,
+                base: baseApp,
+                filename: name+'.js'
+            }, path.join(basePath, module, 'directives'));
+        },
+        directiveTest: function (module, name) {
+            scaffold.compile('directive_test.hbs', {
+                module: module,
+                name: name,
+                base: baseApp,
+                filename: name+'.js'
+            }, path.join(basePathTest, module, 'directives'));
+        },
+        provider: function (module, name) {
+            scaffold.compile('provider.hbs', {
+                module: module,
+                name: name,
+                base: baseApp,
+                filename: name+'.js'
+            }, path.join(basePath, module, 'providers'));
+        },
+        providerTest: function (module, name) {
+            scaffold.compile('provider_test.hbs', {
+                module: module,
+                name: name,
+                base: baseApp,
+                filename: name+'.js'
+            }, path.join(basePathTest, module, 'providers'));
+        }
+    };
+
+    if(gulp.env.type === 'module') {
+        if (fs.existsSync(path.join(basePath, gulp.env.name))) {
+            scaffold.module(gulp.env.name);
+            console.log('Module ' + gulp.env.name + ' already exists');
+        } else {
+            scaffold.module(gulp.env.name);
+            scaffold.index(gulp.env.name);
+            scaffold.controller(gulp.env.name, gulp.env.name);
+            scaffold.controllerTest(gulp.env.name, gulp.env.name);
+            scaffold.template(gulp.env.name, gulp.env.name);
+        }
+    }
+
+    if(gulp.env.type === 'd') {
+        n = scaffold.checkName(gulp.env.name);
+        scaffold.directive(n[0], n[1]);
+        scaffold.directiveTest(n[0], n[1]);
+    }
+
+    if(gulp.env.type === 'p') {
+        n = scaffold.checkName(gulp.env.name);
+        scaffold.provider(n[0], n[1]);
+        scaffold.providerTest(n[0], n[1]);
+    }
+
+    if(gulp.env.type === 'c') {
+        n = scaffold.checkName(gulp.env.name);
+        scaffold.controller(n[0], n[1]);
+        scaffold.controllerTest(n[0], n[1]);
+    }
+
 });
-
-
-// node_modules/.bin/webdriver-manager update
